f7b64ac397361938c0caeafd0b51e67018ddf0d9
Amina Rahimli <emine@rehimli.info>
2019-08-19 16:40:23 +0300

    logger added

---
+++ README.md
@@ -0,0 +1,20 @@
+# Forum server
+
+
+### Installing dependencies
+
+```sh
+npm install
+```
+
+### Running in dev mode
+
+```sh
+npm run dev
+```
+
+### Starting server
+
+```sh
+npm start
+```
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import './utils/dotenv';
 
+const logger = require('./utils/logger')('server');
+
 const app = express();
 
 app.get('/', function (req, res) {
@@ -11,5 +13,5 @@ const host = process.env[`HOST_${process.platform.toUpperCase()}`];
 const port = process.env.PORT || process.env.HOST_PORT;
 
 app.listen(port, host, () => {
-  console.log('info', `App is running at http://${host}:${port} in ${app.get('env')} mode.`);
+  logger.log('info', `App is running at http://${host}:${port} in ${app.get('env')} mode.`);
 });
